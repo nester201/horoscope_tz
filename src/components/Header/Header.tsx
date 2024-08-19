1e9b5773aea207eb654a79a4fd777b2500089443
nester201 <140334604+nester201@users.noreply.github.com>
2024-08-19 19:11:28 +0300

    add heder, context

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import './Header.css';
 import Button from '../Button/Button';
-import { useTelegram } from '../../hooks/useTelegram';
 import { useInitialContext } from '../../hooks/useInitialComtext';
 
 const Header = () => {
   const { currentLanguage } = useInitialContext();
-  const { user } = useTelegram();
 
   return (
     <div className={'header'}>
       <Button text={'Button'} onClick={() => {}} />
-      <span className={'username'}>{user?.username}</span>
+      <span className={'username'}>{''}</span>
       <div>{currentLanguage}</div>
     </div>
   );
