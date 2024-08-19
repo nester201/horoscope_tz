3941a675986765d05fa26bb84bbf4f25eb675604
nester201 <140334604+nester201@users.noreply.github.com>
2024-08-19 17:23:44 +0300

    initial

---
+++ src/bot.js
@@ -0,0 +1,20 @@
+import TelegramBot from 'node-telegram-bot-api';
+
+const token = '7481817295:AAHhcNRXfQ3pFrvoZI7z93TlBeFnwNn5biU';
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match && match[1];
+
+  if (resp) {
+    bot.sendMessage(chatId, resp);
+  }
+});
+
+bot.on('message', msg => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'Received your message');
+});
